7f1cc758043d73e82947aec0d51425363d2b9ecb
sschwaner <stephen.schwaner@gmail.com>
2023-01-11 21:00:53 -0500

    First Commit -- no code yet

---
+++ main.py
@@ -0,0 +1,2 @@
+# I'm going to use beautiful soup or scrapy to pull information from Kids In Mind so that I have a movie database on my computer
+# As a second step, I might do the same thing with decent films so that I can get all of his
