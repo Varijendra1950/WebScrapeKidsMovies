6760e0e1e490a65c4869726e47e55b75ecc7f0c7
sschwaner <stephen.schwaner@gmail.com>
2023-01-23 22:12:28 -0500

    Pull information for "A" page

--- main.py
+++ main.py
@@ -2,15 +2,10 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+import string
 import time
 import urllib.request
-from csv import reader, writer
 
-
-
-start_page = "https://kids-in-mind.com/a.htm"
-
-res = requests.get(start_page)
 
 movie_dict = {"title": [],
               "year": [],
@@ -19,49 +14,65 @@ movie_dict = {"title": [],
               "KIM violence": [],
               "KIM language": []}
 
-if res:
-    soup = BeautifulSoup(res.text, "html.parser")
-    a = soup.findAll("div", class_="et_pb_text_inner")
+for letter in string.ascii_lowercase:
+    url = f"https://kids-in-mind.com/{letter}.htm"
 
-    idx = 0
-    for entry in a:
-        text = entry.getText()
-        if "Movie Reviews by Title" in text:
+    res = requests.get(url)
+
+    if res:
+        soup = BeautifulSoup(res.text, "html.parser")
+        a = soup.findAll("div", class_="et_pb_text_inner")
+
+        idx = 0
+        for entry in a:
+            text = entry.getText()
+            if "Movie Reviews by Title" in text:
+                idx += 1
+                break
             idx += 1
-            break
-        idx += 1
-    # All movies on the page, separated by /n
-    movies = a[idx].getText().split("\n")
+        # All movies on the page, separated by /n
+        movies = a[idx].getText().split("\n")
 
-    # TODO Assemble a movie dictionary
+        for movie in movies:
 
-    for movie in movies:
+            # some entries had a foreign name in brackets
+            if movie.count("]") > 2:
+                start_idx = movie.find("]")+1
+            else:
+                start_idx = 0
 
-        year_idx1 = movie.find("[")
-        year_idx2 = movie.find("]")
+            year_idx1 = movie.find("[", start_idx)
+            year_idx2 = movie.find("]", start_idx)
 
-        mpaa_idx1 = movie.find("[", year_idx1+1)
-        mpaa_idx2 = movie.find("]", year_idx2 + 1)
+            mpaa_idx1 = movie.find("[", year_idx1+1)
+            mpaa_idx2 = movie.find("]", year_idx2 + 1)
 
-        year = int(movie[year_idx1+1:year_idx2].strip())
-        mpaa = movie[mpaa_idx1+1:mpaa_idx2]
+            year = int(movie[year_idx1+1:year_idx2].strip())
+            mpaa = movie[mpaa_idx1+1:mpaa_idx2]
 
-        ratings = movie.split("–")[-1].split(".")
-        ratings = [int(x) for x in ratings]
+            ratings_split = movie.split("–")
+            # sometimes they used a dash, sometimes an en dash
+            if len(ratings_split) == 1:
+                ratings_split = movie.split("-")
 
-        title = movie[0:year_idx1]
+            ratings = [int(x) for x in ratings_split[-1].split(".")]
 
-        movie_dict["title"].append(title)
-        movie_dict["year"].append(year)
-        movie_dict["mpaa"].append(mpaa)
-        movie_dict["KIM sex"].append(ratings[0])
-        movie_dict["KIM violence"].append(ratings[1])
-        movie_dict["KIM language"].append(ratings[2])
+            title = movie[0:year_idx1]
 
-    res.close()
+            movie_dict["title"].append(title)
+            movie_dict["year"].append(year)
+            movie_dict["mpaa"].append(mpaa)
+            movie_dict["KIM sex"].append(ratings[0])
+            movie_dict["KIM violence"].append(ratings[1])
+            movie_dict["KIM language"].append(ratings[2])
 
-else:
-    print(f"Error: {res}")
+        res.close()
+
+        print(f"Done with {letter}. Waiting 0.5 second")
+        time.sleep(0.5)
+
+    else:
+        print(f"Error: {res}")
 
 df_movies = pd.DataFrame(movie_dict)
 
